023150a4252159a09ee4f341610b8ec86e01b955
kimyw1018 <poppppp00@gmail.com>
2026-02-05 10:39:59 +0900

    chore: Tanstack Query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Vite + React + TypeScript</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </header>
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <div>CRAME</div>
+      
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
-
+export default App;
